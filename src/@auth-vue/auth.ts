a91a3b87ec192a4bb47368a3024930e82bd2d36b
Badisi <badisi84@gmail.com>
2024-11-22 15:53:02 +0100

    fix: vue is not waiting for plugin installation if it is async

--- src/@auth-vue/auth.ts
+++ src/@auth-vue/auth.ts
@@ -20,28 +20,35 @@ const DEFAULT_SETTINGS: Optional<AuthSettings, 'authorityUrl' | 'clientId'> = {
   },
 };
 
-export const initAuth = async (settings: AuthSettings): Promise<Plugin> => ({
-  install: async (app: App, { router }: { router: Router }) => {
-    // Effectively adding authService to every component instance
-    const authManager = await initOidc({ ...DEFAULT_SETTINGS, ...settings });
-    const authService = new AuthService(authManager, router);
-    app.provide('$authService', authService);
-    app.config.globalProperties.$authService = authService;
+export const initAuth = async (settings: AuthSettings): Promise<Plugin> => {
+  /**
+   *  Vue do not wait for a plugin to be installed if the installation is async.
+   *  So we have to get the authManager prior of the plugin installation phase, to
+   *  make sur any code that will use it later on, will actually get it.
+   */
+  const authManager = await initOidc({ ...DEFAULT_SETTINGS, ...settings });
+  return {
+    install: (app: App, { router }: { router: Router }) => {
+      // Effectively adding authService to every component instance
+      const authService = new AuthService(authManager, router);
+      app.provide('$authService', authService);
+      app.config.globalProperties.$authService = authService;
 
-    // Add global router guard
-    router.beforeEach((to, _from, next) => {
-      if (to.matched.some((record) => record.meta.requiresAuth)) {
-        watchEffect(async () => {
-          if (authService.isAuthenticatedRef.value) {
-            next();
-          } else {
-            await authService.login({ redirectUrl: to.path });
-            next(false);
-          }
-        });
-      } else {
-        next();
-      }
-    });
-  },
-});
+      // Add global router guard
+      router.beforeEach((to, _from, next) => {
+        if (to.matched.some((record) => record.meta.requiresAuth)) {
+          watchEffect(async () => {
+            if (authService.isAuthenticatedRef.value) {
+              next();
+            } else {
+              await authService.login({ redirectUrl: to.path });
+              next(false);
+            }
+          });
+        } else {
+          next();
+        }
+      });
+    },
+  };
+};
